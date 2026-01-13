0aab063a2a339efc7dabab808a7fca72664f2897
Fabricz20a <Crockbird288@gmail.com>
2026-01-13 16:51:58 +0400

    Update StreetlifeRemasteredakls.lua

--- StreetlifeRemasteredakls.lua
+++ StreetlifeRemasteredakls.lua
@@ -1,4 +1,10 @@
 local UserInputService = game:GetService("UserInputService")
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+repeat task.wait() until player and UserInputService
+
+repeat task.wait() until UserInputService.TouchEnabled ~= nil and UserInputService.KeyboardEnabled ~= nil
 
 if UserInputService.TouchEnabled and not UserInputService.KeyboardEnabled then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Fabricz20a/dfds/refs/heads/main/Streetmob.lua"))()
